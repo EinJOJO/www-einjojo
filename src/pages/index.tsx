0daa0d95ff447e45f0d35b321f2826416406bebf
Johannes / EinJojo <62117829+EinJOJO@users.noreply.github.com>
2022-04-15 15:47:13 +0200

    hidden below sm

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,8 +6,8 @@ const Home: NextPage = () => {
     <>
       <div className="bg-gradient-to-b from-primary-bg to-secondary-bg text-white">
         <div className="h-screen">
-          <div className="flex justify-center items-center bg-hero-pattern text-center bg-center bg-no-repeat h-full">
-            <div className="bg-black/90 px-10 py-5">
+          <div className="flex justify-center items-center sm:bg-hero-pattern text-center bg-center bg-no-repeat h-full">
+            <div className="sm:bg-black/90 sm:rounded-md px-10 py-5">
               <h1>Hello</h1>
             </div>
           </div>
